bb129ed09e35370d7c88528892c2200e130dabfc
Agnieszka Janiak <agnieszka.janiakk@gmail.com>
2017-06-22 12:31:42 +0200

    add highlight

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWduaWVzemtham90IiwiYSI6ImNqMDloMDFscTAwM3UycW14ZnM1YmFvZm0ifQ.cQxnfJqnE2yCYL9MGO89aw';
-var mapka = new mapboxgl.Map({
+var map = new mapboxgl.Map({
     container: 'mapka', // id kontenera (div) mapy
         style: 'mapbox://styles/agnieszkajot/cj485493d1o182slb8auqq058', //lokalizacja pliku stylów
             center: [16.8752446, 52.4079914], // centrum mapy
         zoom: 2 // bazowy zoom 
 });
-mapka.addControl(new mapboxgl.ScaleControl({
+map.addControl(new mapboxgl.ScaleControl({
     maxWidth: 80,
     unit: 'imperial'
 
@@ -15,14 +15,38 @@ function dataToString(x) {
 	return x.from + ": " + x.number;
 }
 
-mapka.on('load', function() {
-  mapka.addLayer({
+function getFilteredData(e) {
+	country = e.features[0].properties.NAME;
+	function f(x) {
+		return x.to == country;
+	}
+	var filtered = data.filter(f);
+	return filtered;
+}
+
+map.on('load', function() {
+   map.addSource("data", {
+        "type": "vector",
+         url: 'mapbox://agnieszkajot.cj486gvg8090n2wlfwr62oawi-075p4'
+    });
+  map.addLayer({
     id: 'kartokraje',
             type: 'fill',
-            source: {
-                  type: 'vector',
-                  url: 'mapbox://agnieszkajot.cj486gvg8090n2wlfwr62oawi-075p4'
+            source: 'data',
+            'source-layer': 'kartokraje',
+            layout: {
+                  visibility: 'visible'
                     },
+            paint: {
+                 'fill-color': 'rgba(200, 100, 240, 0.2)',
+            'fill-outline-color': 'rgba(200, 100, 240, 1)'
+                    }
+          });
+		
+	map.addLayer({
+    id: 'kraje_highlight',
+            type: 'fill',
+            source: 'data',
             'source-layer': 'kartokraje',
             layout: {
                   visibility: 'visible'
@@ -30,21 +54,22 @@ mapka.on('load', function() {
             paint: {
                  'fill-color': 'rgba(200, 100, 240, 0.4)',
             'fill-outline-color': 'rgba(200, 100, 240, 1)'
-                    }
+                    },
+			"filter": ["==", "name", ""]
           });
 		
 		
 		
-	 mapka.on('click', 'kartokraje', function (e) {
-		country = e.features[0].properties.NAME;
-		function f(x) {
-			return x.to == country;
-		}
-		var filtered = data.filter(f);
+	 map.on('click', 'kartokraje', function (e) {
+		var filtered = getFilteredData(e);
 		var strings = filtered.map(dataToString);
         new mapboxgl.Popup()
             .setLngLat(e.lngLat)
             .setHTML(strings.join("<br>"))
-            .addTo(mapka);
+            .addTo(map);
+		map.setFilter("kraje_highlight", ["in", "NAME"].concat(filtered.map(e => e.from)).concat(e.features[0].properties.NAME));
+
     });
+	
+	 
 });
